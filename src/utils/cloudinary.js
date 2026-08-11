d045b7a0f511532d6111ae9ad49b24443464446a
Aditya Sutar <sutaraditya2807@gmail.com>
2026-08-11 15:49:17 +0530

    user controller fix

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -11,7 +11,8 @@ const uploadOnCloudinary = async (localFilePath) => {
     try {
         if (!localFilePath) return null
             const response = await cloudinary.uploader.upload(localFilePath, {resource_type: 'auto'})
-            console.log("file is uploaded successfully ", response.url);
+            // console.log("file is uploaded successfully ", response.url);
+            fs.unlinkSync(localFilePath)
             return response;
     } catch (error) {
         fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed 
